9f9bf62f9e457025d4928e59f66fcd823de5b43f
willyngash <willynganga024@gmail.com>
2021-01-23 19:50:19 +0300

    Intial Commit

---
+++ src/main/java/ke/co/willynganga/tusomebackend/TusomeBackendApplication.java
@@ -0,0 +1,13 @@
+package ke.co.willynganga.tusomebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TusomeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TusomeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ke/co/willynganga/tusomebackend/TusomeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ke.co.willynganga.tusomebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TusomeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

64df2f9bce99eeef158ad0138782845212c15118
willyngash <willynganga024@gmail.com>
2021-01-23 20:53:36 +0300

    Included custom finders

--- src/main/java/ke/co/willynganga/tusomebackend/repositories/PaperRepository.java
+++ src/main/java/ke/co/willynganga/tusomebackend/repositories/PaperRepository.java
@@ -1,7 +1,17 @@
 package ke.co.willynganga.tusomebackend.repositories;
 
 import ke.co.willynganga.tusomebackend.models.Paper;
+import ke.co.willynganga.tusomebackend.other.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PaperRepository extends JpaRepository<Paper, Long> {
+
+    List<Paper> findPapersByYear(int year);
+
+    List<Paper> findPapersByPaperCategory(Category category);
+
+    List<Paper> findPapersByTitle(String title);
+
 }

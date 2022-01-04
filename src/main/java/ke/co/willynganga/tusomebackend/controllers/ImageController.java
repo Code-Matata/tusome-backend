30753ac69f45700dbe3efdb6b828c6761c9de164
willyngash <willynganga024@gmail.com>
2022-01-04 19:30:53 +0300

    Add api documentation

--- src/main/java/ke/co/willynganga/tusomebackend/controllers/ImageController.java
+++ src/main/java/ke/co/willynganga/tusomebackend/controllers/ImageController.java
@@ -1,9 +1,11 @@
 package ke.co.willynganga.tusomebackend.controllers;
 
+import io.swagger.annotations.ApiOperation;
 import ke.co.willynganga.tusomebackend.models.Image;
 import ke.co.willynganga.tusomebackend.services.ImageService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequestMapping("api/v1/images")
@@ -16,6 +18,10 @@ public class ImageController {
         this.imageService = imageService;
     }
 
+    @ApiOperation(
+            value = "Get image by id.",
+            notes = "Download image with given id from DB."
+    )
     @GetMapping(
             path = "/getImage/{id}",
             produces = MediaType.IMAGE_JPEG_VALUE
@@ -29,6 +35,7 @@ public class ImageController {
         }
     }
 
+    @ApiIgnore
     @DeleteMapping("deleteImage/{id}")
     public String deleteImage(@PathVariable("id") long id) {
         return imageService.deleteImage(id);

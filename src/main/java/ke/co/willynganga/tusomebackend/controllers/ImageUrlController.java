a17ef9d5258ef053ee479b828de1bb9605b86771
willyngash <willynganga024@gmail.com>
2022-01-04 19:30:50 +0300

    Add api documentation

--- src/main/java/ke/co/willynganga/tusomebackend/controllers/ImageUrlController.java
+++ src/main/java/ke/co/willynganga/tusomebackend/controllers/ImageUrlController.java
@@ -1,12 +1,11 @@
 package ke.co.willynganga.tusomebackend.controllers;
 
+import io.swagger.annotations.ApiOperation;
 import ke.co.willynganga.tusomebackend.services.ImageUrlService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1")
 @CrossOrigin
 public class ImageUrlController {
 
@@ -16,6 +15,10 @@ public class ImageUrlController {
         this.urlService = urlService;
     }
 
+    @ApiOperation(
+            value = "Get image url by id.",
+            notes = "Fetch image url with provided id in DB."
+    )
     @GetMapping("/{id}")
     public String getUrl(@PathVariable("id") Long id) {
         return urlService.getUrl(id);

20d5671a0babe9c22976f4f349c1242fb066a5b5
willyngash <willynganga024@gmail.com>
2022-01-04 19:44:14 +0300

    Update PaperDTO.java

--- src/main/java/ke/co/willynganga/tusomebackend/models/PaperDTO.java
+++ src/main/java/ke/co/willynganga/tusomebackend/models/PaperDTO.java
@@ -2,10 +2,12 @@ package ke.co.willynganga.tusomebackend.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
